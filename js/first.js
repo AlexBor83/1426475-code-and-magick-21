2654c0c7d069a1c816952da893403e8388c639ba
AlexBor83 <rabota_for_alex@mail.ru>
2020-09-22 23:54:45 +0400

    исправил ошибки

--- js/first.js
+++ js/first.js
@@ -1,15 +1,23 @@
 "use strict";
 
 var fireballSize = 22;
+
 var getFireballSpeed = function (isLeft) {
   return isLeft ? 2 : 5;
 }
+
 var wizardSpeed = 3;
+
 var wizardWidth = 70;
-var getWizardHeight = function (wizardHeight) {return 1.337*wizardWidth};
-var getWizardX = function (gameWizardX) {
-  return (gameWizardX-wizardWidth)/2;
+
+var getWizardHeight = function (wizardHeight) {
+  return 1.337 * wizardWidth;
 }
+
+var getWizardX = function (gameWizardX) {
+  return (gameWizardX-wizardWidth) / 2;
+}
+
 var getWizardY = function (gameWizardy) {
-  return gameWizardy/3;
+  return gameWizardy / 3;
 }
